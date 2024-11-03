e632be740ecb80b6bb97b2a355e37d304d4222f7
Yao ShuMin <roller44@163.com>
2024-11-03 23:47:23 +0800

    Update test.m

--- test.m
+++ test.m
@@ -1,9 +1,13 @@
-clear;
-clc;
-
-y = RegularizedIncompleteBetaFun(1, 2, 0);
-
-function y = RegularizedIncompleteBetaFun(a, b, upper)
-    betaFun = @(t) t^(a-1) .* (1-t).^(b-1);
-    y = integral(betaFun, 0, upper) ./ beta(a, b);
+numSamples = 16;
+ORSCorrectCases = [];
+for numQ1 = 0: 1: numSamples
+    for numQ2 = 0: 1: (numSamples-numQ1)
+        for numQ3 = 0: 1: (numSamples-numQ1-numQ2)
+            numQ4 = numSamples - numQ3 - numQ2 - numQ1;
+            if (numQ1>=numQ2) && (numQ1>=numQ3) && (numQ1>=numQ4)
+                % Only consider the case where samples fall in the first quadrant are the most.
+                ORSCorrectCases = [ORSCorrectCases; [numQ1, numQ2, numQ3, numQ4]];
+            end
+        end
+    end
 end
