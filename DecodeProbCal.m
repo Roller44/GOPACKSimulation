ee4c643f7a7f7e68c0c1fbe2743036806a858e14
Shumin Yao <roller44@163.com>
2024-11-11 15:00:32 +0800

    Delete unused code

--- DecodeProbCal.m
+++ DecodeProbCal.m
@@ -24,7 +24,7 @@ global signalPower_dBm;
         sampleImag = imag(samples);
 
         % Calculate noise power
-        % dBm = dBW + 30 
+        dBm = dBW + 30 
         noisePower_mW = db2pow(signalPower_dBm - SNR_dB - 30) / 2;
         % An alternative approach
         % signalPower_mW = samples * samples' / length(samples);
@@ -32,23 +32,11 @@ global signalPower_dBm;
         % noisePower_mW = signalPower_mW / SNR_linear / 2;
         
 
-        % The lower bound and uper bound of each quandrant
-        % The ith element of list corresponds to the quandrant i.
-        % lowBoundList = [0, pi/2, pi, (3/2)*pi];
-        % upBoundList = [pi/2, pi, (3/2)*pi, 2*pi];
-        % quaProb = zeros(4, numSampPerWave);
-        % for samp_ith = 1: 1: size(samples, 2)
-        %     fun = @(x, y) (x./(2*pi*noisePower_mW)) .*...
-        %             exp(-((x.*cos(y) - sampleReal(samp_ith)).^2 + (x.*sin(y) - sampleImag(samp_ith)).^2) ./ (2.*noisePower_mW));
-        % 
-        %     quaProb(1, samp_ith) = integral2(fun, 0, Inf, lowBoundList(1, message), upBoundList(1, message));
-        % end
-
         quaProb = zeros(4, numSampPerWave);
         for samp_ith = 1: 1: size(samples, 2)
             fun = @(x, y) (1./(2*pi*noisePower_mW)) .*...
                     exp(-((x - sampleReal(samp_ith)).^2 + (y - sampleImag(samp_ith)).^2) ./ (2*noisePower_mW));
-            
+
             quaProb(1, samp_ith) = integral2(fun, 0, Inf, 0, Inf);
             quaProb(2, samp_ith) = integral2(fun, -Inf, 0, 0, Inf);
             quaProb(4, samp_ith) = integral2(fun, 0, Inf, -Inf, 0);
@@ -58,69 +46,63 @@ global signalPower_dBm;
 
     end
 
-ACKCorrectCases = settings.ACKCorrectCases;
-message = 1;
-sampQuaProb = QuaProbCal(message, offset, numORS, lenType, SNR_dB, RXType);
-
-if isequal(RXType, 'BLE')
-    results.corrQuaDecodeProb = sampQuaProb(1, 1);
-    results.corrORSDecodeProb = sampQuaProb(1, 1);
-    quaProb = sampQuaProb';
-else
-
-    results.corrQuaDecodeProb = sampQuaProb(1, 3);
-
-    meanProb = sum(sampQuaProb, 2)';
+    ACKCorrectCases = settings.ACKCorrectCases;
+    message = 1;
+    sampQuaProb = QuaProbCal(message, offset, numORS, lenType, SNR_dB, RXType);
     
-    varProb = diag(sampQuaProb * (1 - sampQuaProb)')';
+    if isequal(RXType, 'BLE')
+        results.corrQuaDecodeProb = sampQuaProb(1, 1);
+        results.corrORSDecodeProb = sampQuaProb(1, 1);
+        quaProb = sampQuaProb';
+    else
     
-    coVarProb = zeros(4, 4);
-    meanProbZ = zeros(4, 4);
-    varProbZ = zeros(4, 4);
-    quaProb = zeros(4, 4);
-    for qua_ith_1 = 1: 4
-        for qua_ith_2 = 1: 4
-            if qua_ith_1 ~= qua_ith_2
-                coVarProb(qua_ith_1, qua_ith_2) = -1 * sampQuaProb(qua_ith_1, :) * sampQuaProb(qua_ith_2, :)';
-                meanProbZ(qua_ith_1, qua_ith_2) = meanProb(1, qua_ith_1) - meanProb(1, qua_ith_2);
-                varProbZ(qua_ith_1, qua_ith_2) = varProb(1, qua_ith_1) + varProb(1, qua_ith_2) - 2 * coVarProb(qua_ith_1, qua_ith_2);
-                if varProbZ(qua_ith_1, qua_ith_2) < 0
-                     error('Negative variance encountered. Check sampQuaProb for validity.');
+        results.corrQuaDecodeProb = sampQuaProb(1, 3);
+    
+        meanProb = sum(sampQuaProb, 2)';
+        
+        varProb = diag(sampQuaProb * (1 - sampQuaProb)')';
+        
+        coVarProb = zeros(4, 4);
+        meanProbZ = zeros(4, 4);
+        varProbZ = zeros(4, 4);
+        quaProb = zeros(4, 4);
+        for qua_ith_1 = 1: 4
+            for qua_ith_2 = 1: 4
+                if qua_ith_1 ~= qua_ith_2
+                    coVarProb(qua_ith_1, qua_ith_2) = -1 * sampQuaProb(qua_ith_1, :) * sampQuaProb(qua_ith_2, :)';
+                    meanProbZ(qua_ith_1, qua_ith_2) = meanProb(1, qua_ith_1) - meanProb(1, qua_ith_2);
+                    varProbZ(qua_ith_1, qua_ith_2) = varProb(1, qua_ith_1) + varProb(1, qua_ith_2) - 2 * coVarProb(qua_ith_1, qua_ith_2);
+                    if varProbZ(qua_ith_1, qua_ith_2) < 0
+                         error('Negative variance encountered. Check sampQuaProb for validity.');
+                    end
+                    quaProb(qua_ith_1, qua_ith_2) = 1 - normcdf(0, meanProbZ(qua_ith_1, qua_ith_2), sqrt(varProbZ(qua_ith_1, qua_ith_2)));
+                else
+                    quaProb(qua_ith_1, qua_ith_2) = 0;
                 end
-                quaProb(qua_ith_1, qua_ith_2) = 1 - normcdf(0, meanProbZ(qua_ith_1, qua_ith_2), sqrt(varProbZ(qua_ith_1, qua_ith_2)));
-            else
-                quaProb(qua_ith_1, qua_ith_2) = 0;
             end
         end
-    end
-    
-    probTmp = ones(1, 4);
-    for qua_ith_1 = 1: 1: 4
-        for qua_ith_2 = 1: 1: 4
-            if qua_ith_1 ~= qua_ith_2
-                probTmp(1, qua_ith_1) = probTmp(1, qua_ith_1) * quaProb(qua_ith_1, qua_ith_2);
+        
+        probTmp = ones(1, 4);
+        for qua_ith_1 = 1: 1: 4
+            for qua_ith_2 = 1: 1: 4
+                if qua_ith_1 ~= qua_ith_2
+                    probTmp(1, qua_ith_1) = probTmp(1, qua_ith_1) * quaProb(qua_ith_1, qua_ith_2);
+                end
             end
         end
+        
+        % Compensate approximation errors.
+        tmp = 1 - sum(probTmp);
+        quaProb = probTmp + tmp./size(probTmp, 2);
+        results.corrORSDecodeProb = quaProb(1, 1);
+        
     end
     
-    % Compensate approximation errors.
-    tmp = 1 - sum(probTmp);
-    quaProb = probTmp + tmp./size(probTmp, 2);
-    results.corrORSDecodeProb = quaProb(1, 1);
-    
-end
-
-numACKCorrectCases = size(ACKCorrectCases, 1);
-% offsetValue = offset.min: 1: offset.max;
-% offsetValue = offset.offsetValue;
-% message = 1;
-% quaProb = QuaProbCal(message, offsetValue, numORS, lenType, SNR_dB, RXType);
-probTmp = 0;
-for case_ith = 1: 1: numACKCorrectCases
-    probTmp = probTmp + mnpdf(ACKCorrectCases(case_ith, :), quaProb);
-end
-% corrACKDecodeProb = probTmp ./ (offset.max - offset.min);
-corrACKDecodeProb = probTmp;
-results.corrACKDecodeProb = corrACKDecodeProb;
+    numACKCorrectCases = size(ACKCorrectCases, 1);
+    probTmp = 0;
+    for case_ith = 1: 1: numACKCorrectCases
+        probTmp = probTmp + mnpdf(ACKCorrectCases(case_ith, :), quaProb);
+    end
+    results.corrACKDecodeProb = probTmp;
 
 end
